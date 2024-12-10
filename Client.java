32b54c99cd8208b04ffabc4d44cedbe45fe40f6e
Harshit <harshitrocking9407@gmail.com>
2024-12-10 18:44:21 +0530

    client gui

--- Client.java
+++ Client.java
@@ -1,15 +1,33 @@
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-class Client{
+class Client extends JFrame{
 
     Socket socket;
 
     BufferedReader reader;
     PrintWriter writer;
+
+    private JLabel heading = new JLabel("Client Area");
+    private JTextArea messageArea = new JTextArea();
+    private JTextField messageInput = new JTextField();
+    private Font font = new Font("Roboto", Font.PLAIN, 20);
+
 
     public Client(){
         try {
@@ -20,13 +38,72 @@ class Client{
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             writer = new PrintWriter(socket.getOutputStream());
 
+            createGUI();
+            handleEvents();
+
             startReading();
-            startWriting();
+            // startWriting();
 
         } catch (Exception e) {
             // e.printStackTrace();
             System.out.println("Connection is closed");
         }
+    }
+
+    private void createGUI(){
+        this.setTitle("Client Messager[END]");
+        this.setSize(500,600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        messageInput.setFont(font);
+
+        messageArea.setFont(font);
+        messageArea.setEditable(false);
+        JScrollPane scrollPaneMessageArea = new JScrollPane(messageArea);
+
+
+        // heading.setIcon(new ImageIcon("img.png"));
+        heading.setFont(font);
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading.setHorizontalTextPosition(SwingConstants.CENTER);
+        heading.setVerticalTextPosition(SwingConstants.BOTTOM);
+        heading.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+
+        this.setLayout(new BorderLayout());
+        this.add(heading, BorderLayout.NORTH);
+        this.add(scrollPaneMessageArea, BorderLayout.CENTER);
+        this.add(messageInput, BorderLayout.SOUTH);
+
+
+        this.setVisible(true);
+    }
+
+    private void handleEvents(){
+        messageInput.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if( e.getKeyCode() == 10){
+                    // System.out.println("You have pressed enter");
+                    String contentToSend = messageInput.getText();
+                    messageArea.append("Me : " + contentToSend + "\n");
+                    writer.println(contentToSend);
+                    writer.flush();
+                    messageInput.setText("");
+                }
+            }
+            
+        });
     }
 
     public void startReading(){
@@ -38,10 +115,13 @@ class Client{
                     String msg = reader.readLine();
                     if(msg.equals("exit")){
                         System.out.println("Server terminated the chat");
+                        JOptionPane.showMessageDialog(this, "Server termonated the chat");
+                        messageInput.setEnabled(false);
                         socket.close();
                         break;
                     }
-                    System.out.println("Server : " + msg);
+                    // System.out.println("Server : " + msg);
+                    messageArea.append("Server : " + msg + "\n");
                 }
             } catch (Exception e) {
                 // e.printStackTrace();
